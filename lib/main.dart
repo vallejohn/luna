9ba7f100ee1e51da39a6ee150499add514d4a436
vallejohn <valjohn.teruel@gmail.com>
2021-09-26 21:42:49 +0800

    implemented StatusbarzCapturer

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,32 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:luna/app/app.locator.dart';
 import 'package:luna/app/app.router.dart';
-import 'package:luna/global/styles.dart';
 import 'package:stacked_services/stacked_services.dart';
+import 'package:statusbarz/statusbarz.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   setupLocator();
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-    statusBarColor: lPrimaryColor.withOpacity(0),
-  ));
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        fontFamily: 'Rubik',
-        primarySwatch: Colors.blue,
+    return StatusbarzCapturer(
+      child: MaterialApp(
+        navigatorObservers: [Statusbarz.instance.observer],
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          fontFamily: 'Rubik',
+          primarySwatch: Colors.blue,
+        ),
+        navigatorKey: StackedService.navigatorKey,
+        onGenerateRoute: StackedRouter().onGenerateRoute,
       ),
-      navigatorKey: StackedService.navigatorKey,
-      onGenerateRoute: StackedRouter().onGenerateRoute,
     );
   }
 }
