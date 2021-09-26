6bcfba4b540208818764e5feeff53f2042cc9761
vallejohn <valjohn.teruel@gmail.com>
2021-09-26 19:37:38 +0800

    Added transparent statusbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:luna/app/app.locator.dart';
 import 'package:luna/app/app.router.dart';
+import 'package:luna/global/styles.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   setupLocator();
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: lPrimaryColor.withOpacity(0),
+  ));
   runApp(MyApp());
 }
 
