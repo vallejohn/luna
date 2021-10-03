1a4034b9c9dd298066e21f71fad11427361d50bb
vallejohn <valjohn.teruel@gmail.com>
2021-10-03 11:54:18 +0800

    major updates for register form view

--- lib/ui/auth/register/register_view.dart
+++ lib/ui/auth/register/register_view.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:ionicons/ionicons.dart';
-import 'package:luna/global/custom_widgets/shadowed_text_button.dart';
 import 'package:luna/global/styles.dart';
 import 'package:luna/global/ui_helpers.dart';
+import 'package:luna/ui/auth/register/register_form_view.dart';
 import 'package:luna/ui/auth/register/register_viewmodel.dart';
 import 'package:stacked/stacked.dart';
 
@@ -11,183 +10,62 @@ class RegisterView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    TextEditingController usernameController = TextEditingController();
-    TextEditingController firstnameController = TextEditingController();
-    TextEditingController lastnameController = TextEditingController();
-    TextEditingController emailController = TextEditingController();
-    TextEditingController passwordController = TextEditingController();
-
     return ViewModelBuilder<RegisterViewModel>.reactive(
-        viewModelBuilder: () => RegisterViewModel(),
-        builder: (context, model, child) => Scaffold(
-          backgroundColor: Colors.white,
-          body: SafeArea(
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  verticalSpaceMassive,
-                  Center(
-                      child: Text(
-                        'Register',
-                        style: massiveTextStyle,
-                      )),
-                  verticalSpaceLarge,
-                  Padding(
-                    padding:
-                    const EdgeInsets.symmetric(horizontal: horizontalMargin),
-                    child: TextFormField(
-                      controller: usernameController,
-                      textInputAction: TextInputAction.done,
-                      style: TextStyle(
-                          fontSize: textSizeMedium, color: lBodyTextColor),
-                      textAlign: TextAlign.start,
-                      decoration: InputDecoration(
-                        prefixIcon: Icon(
-                          Ionicons.person,
-                          color: lOtherColor,
-                        ),
-                        contentPadding:
-                        EdgeInsets.only(left: 50, top: 10, bottom: 10),
-                        hintText: 'Username',
-                        filled: true,
-                        fillColor: lTFBackgroundColor,
-                        hintStyle: TextStyle(color: lOtherColor),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(customBorderRadius),
-                            borderSide: BorderSide.none),
-                      ),
-                    ),
+      viewModelBuilder: () => RegisterViewModel(),
+      builder: (context, model, child) => Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [lPrimaryColor, lPrimaryColorsTransition]),
+          ),
+          child: ListView(
+            children: [
+              verticalSpaceMassive,
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 45),
+                  child: Text(
+                    'Create an account',
+                    style: largeTextStyle.copyWith(
+                        color: Colors.white,
+                        fontSize: 24,
+                        fontWeight: rubikMedium),
                   ),
-                  verticalSpaceRegular,
-                  Padding(
-                    padding:
-                    const EdgeInsets.symmetric(horizontal: horizontalMargin),
-                    child: TextFormField(
-                      controller: firstnameController,
-                      textInputAction: TextInputAction.done,
-                      style: TextStyle(
-                          fontSize: textSizeMedium, color: lBodyTextColor),
-                      textAlign: TextAlign.start,
-                      decoration: InputDecoration(
-                        prefixIcon: Icon(
-                          Ionicons.person,
-                          color: lOtherColor,
-                        ),
-                        contentPadding:
-                        EdgeInsets.only(left: 50, top: 10, bottom: 10),
-                        hintText: 'First Name',
-                        filled: true,
-                        fillColor: lTFBackgroundColor,
-                        hintStyle: TextStyle(color: lOtherColor),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(customBorderRadius),
-                            borderSide: BorderSide.none),
-                      ),
-                    ),
-                  ),
-                  verticalSpaceRegular,
-                  Padding(
-                    padding:
-                    const EdgeInsets.symmetric(horizontal: horizontalMargin),
-                    child: TextFormField(
-                      controller: lastnameController,
-                      textInputAction: TextInputAction.done,
-                      style: TextStyle(
-                          fontSize: textSizeMedium, color: lBodyTextColor),
-                      textAlign: TextAlign.start,
-                      decoration: InputDecoration(
-                        prefixIcon: Icon(
-                          Ionicons.person,
-                          color: lOtherColor,
-                        ),
-                        contentPadding:
-                        EdgeInsets.only(left: 50, top: 10, bottom: 10),
-                        hintText: 'Last Name',
-                        filled: true,
-                        fillColor: lTFBackgroundColor,
-                        hintStyle: TextStyle(color: lOtherColor),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(customBorderRadius),
-                            borderSide: BorderSide.none),
-                      ),
-                    ),
-                  ),
-                  verticalSpaceRegular,
-                  Padding(
-                    padding:
-                    const EdgeInsets.symmetric(horizontal: horizontalMargin),
-                    child: TextFormField(
-                      controller: emailController,
-                      textInputAction: TextInputAction.done,
-                      style: TextStyle(
-                          fontSize: textSizeMedium, color: lBodyTextColor),
-                      textAlign: TextAlign.start,
-                      decoration: InputDecoration(
-                        prefixIcon: Icon(
-                          Ionicons.person,
-                          color: lOtherColor,
-                        ),
-                        contentPadding:
-                        EdgeInsets.only(left: 50, top: 10, bottom: 10),
-                        hintText: 'Email',
-                        filled: true,
-                        fillColor: lTFBackgroundColor,
-                        hintStyle: TextStyle(color: lOtherColor),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(customBorderRadius),
-                            borderSide: BorderSide.none),
-                      ),
-                    ),
-                  ),
-                  verticalSpaceRegular,
-                  Padding(
-                    padding:
-                    const EdgeInsets.symmetric(horizontal: horizontalMargin),
-                    child: TextFormField(
-                      obscureText: true,
-                      controller: passwordController,
-                      textInputAction: TextInputAction.done,
-                      style: TextStyle(
-                          fontSize: textSizeMedium, color: lBodyTextColor),
-                      textAlign: TextAlign.start,
-                      decoration: InputDecoration(
-                        prefixIcon: Icon(
-                          Ionicons.shield,
-                          color: lOtherColor,
-                        ),
-                        contentPadding:
-                        EdgeInsets.only(left: 50, top: 10, bottom: 10),
-                        hintText: 'Password',
-                        filled: true,
-                        fillColor: lTFBackgroundColor,
-                        hintStyle: TextStyle(color: lOtherColor),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(customBorderRadius),
-                            borderSide: BorderSide.none),
-                      ),
-                    ),
-                  ),
-                  verticalSpaceRegular,
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: horizontalMargin),
-                    child: ShadowedTextButton(
-                      onPressed: () => model.register(
-                        username: usernameController.text.toString(),
-                        firstname: firstnameController.text.toString(),
-                        lastname: lastnameController.text.toString(),
-                        email: emailController.text.toString(),
-                        password: passwordController.text.toString()
-                      ),
-                      backgroundColor: lPrimaryColor,
-                      child: !model.isBusy ? Text('Register', style: mediumTextStyle.copyWith(color: Colors.white),)
-                          : Container(width: 15, height: 15, child: CircularProgressIndicator(color: Colors.white, strokeWidth: 2, )),),
-                  ),
-                ],
+                ),
               ),
-            ),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 45),
+                  child: Text(
+                    'Please provide the required information below.',
+                    style: mediumTextStyle.copyWith(
+                        color: Colors.white, fontWeight: rubikLight),
+                  ),
+                ),
+              ),
+              verticalSpaceLarge,
+              verticalSpaceLarge,
+              /* Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 45),
+                  child: Center(
+                      child: Text(
+                    'Invalid input',
+                    style: mediumTextStyle.copyWith(
+                        color: Colors.white, fontWeight: rubikLight),
+                  )),
+                ), */
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 45),
+                child: RegisterFormView(),
+              ),
+              verticalSpaceRegular,
+            ],
           ),
         ),
+      ),
     );
   }
 }
--- lib/ui/auth/register/register_viewmodel.dart
+++ lib/ui/auth/register/register_viewmodel.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:logger/logger.dart';
 import 'package:luna/app/app.locator.dart';
 import 'package:luna/app/app.router.dart';
 import 'package:luna/models/user_profile.dart';
@@ -9,42 +10,49 @@ import 'package:luna/services/user_profile_service.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
-class RegisterViewModel extends BaseViewModel{
+class RegisterViewModel extends BaseViewModel {
   final _firebaseAuthService = locator<FirebaseAuthService>();
   final _firestoreService = locator<FirestoreService>();
   final _navigationService = locator<NavigationService>();
   final _userProfileService = locator<UserProfileService>();
 
-  void register({
-    required String username,
-    required String firstname,
-    required String lastname,
-    required String email,
-    required String password}) async{
+  final Logger logger = Logger();
 
-    setBusy(true);
-    await _firebaseAuthService.createUserWithEmailAndPassword(
-        email: email,
-        password: password
-    ).then((User? user) async{
-      if(user != null){
-        await _firestoreService.addUserToCollection(UserProfile(
-          authID: user.uid,
-          username: username,
-          email: email,
-          firstname: firstname,
-          lastname: lastname,
-        )).then((DocumentReference documentReference) {
-          documentReference.get().then((DocumentSnapshot documentSnapshot) {
-            UserProfile userProfile = documentSnapshot.data() as UserProfile;
-            _userProfileService.setCurrentUser(userProfile);
+  void register({required String username, required String firstname, required String lastname, required String email, required String password}) async {
+    await runBusyFuture(_firebaseAuthService.createUserWithEmailAndPassword(email: email, password: password)).then((List value) async {
+      UserCredential? userCredential = value[0];
+      FirebaseAuthException? error = value[1];
+
+      if (error != null) {
+        if (error.code == 'weak-password') {
+          logger.w('Weak password');
+        } else if (error.code == 'email-already-in-use') {
+          logger.w('Email already exist!');
+        } else {
+          logger.i('Credentials are validated.');
+        }
+      } else {
+        logger.i('No FirebaseAuthException errors found');
+        if (userCredential != null) {
+          await _firestoreService
+              .addUserToCollection(UserProfile(
+            authID: userCredential.user!.uid,
+            username: 'username',
+            email: email,
+            firstname: firstname,
+            lastname: lastname,
+          ))
+              .then((DocumentReference documentReference) {
+            documentReference.get().then((DocumentSnapshot documentSnapshot) {
+              UserProfile userProfile = documentSnapshot.data() as UserProfile;
+              _userProfileService.setCurrentUser(userProfile);
+            });
           });
-        });
-        await _navigationService.replaceWith(Routes.homeView);
-      }else{
-        print('Error while registering user');
+          await _navigationService.replaceWith(Routes.homeView);
+        } else {
+          logger.i('Error while registering user');
+        }
       }
     });
-    setBusy(false);
   }
 }
