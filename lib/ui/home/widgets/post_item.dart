a8df0d9d46f8d48028fcc2bca0d9bac6f5fad8e0
vallejohn <valjohn.teruel@gmail.com>
2021-10-10 23:42:44 +0800

    Major UI changes on HomeView

--- lib/ui/home/widgets/post_item.dart
+++ lib/ui/home/widgets/post_item.dart
@@ -1,5 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/widgets.dart';
 import 'package:ionicons/ionicons.dart';
 import 'package:luna/global/custom_widgets/shadowed_container.dart';
 import 'package:luna/global/styles.dart';
@@ -19,25 +21,24 @@ class PostItem extends StatelessWidget {
   final String imageUrl;
   final VoidCallback onPostTap;
 
-  const PostItem({Key? key,
-    required this.title,
-    required this.content,
-    required this.commentCount,
-    this.recentComment,
-    required this.imageUrl,
-    required this.name,
-    required this.category,
-    required this.datePosted,
-    required this.onPostTap, 
-    required this.coverImageURL
-  }) : super(key: key);
+  const PostItem(
+      {Key? key,
+      required this.title,
+      required this.content,
+      required this.commentCount,
+      this.recentComment,
+      required this.imageUrl,
+      required this.name,
+      required this.category,
+      required this.datePosted,
+      required this.onPostTap,
+      required this.coverImageURL})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
     double hPadding = 20;
     double vPadding = 10;
-
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 5),
       child: ShadowedContainer(
@@ -45,115 +46,176 @@ class PostItem extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Padding(
-              padding: EdgeInsets.only(left: hPadding, right: hPadding, top: vPadding),
-              child: Row(
-                children: [
-                  CircleAvatar(
-                    backgroundImage: NetworkImage(imageUrl),
+            Stack(
+              children: [
+                Container(
+                  height: 300,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(10)),
+                    image: DecorationImage(
+                        image: NetworkImage('$coverImageURL'),
+                        fit: BoxFit.cover),
                   ),
-                  horizontalSpaceSmall,
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Wrap(
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10)),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 15.0, sigmaY: 15.0),
+                    child: Container(
+                      height: 50,
+                      color: Colors.black.withOpacity(0.4),
+                      child: Padding(
+                        padding:
+                            EdgeInsets.only(left: hPadding, right: hPadding),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
                           children: [
-                            Text(name, style: mediumTextStyle.copyWith(fontWeight: rubikMedium),),
-                            Text(' on ', style: mediumTextStyle.copyWith(color: lLightGrey),),
-                            Text(category, style: mediumTextStyle.copyWith(color: lPrimaryColor, fontWeight: rubikMedium),),
+                            CircleAvatar(
+                              radius: 15,
+                              backgroundImage: NetworkImage(imageUrl),
+                            ),
+                            horizontalSpaceSmall,
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    name,
+                                    style: mediumTextStyle.copyWith(
+                                        color: Colors.white,
+                                        fontWeight: rubikMedium),
+                                  ),
+                                  Text(
+                                    datePosted,
+                                    style: smallTextStyle.copyWith(
+                                        color: Colors.white,
+                                        fontWeight: rubikLight),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text(
+                                  'Okinawa',
+                                  style: smallTextStyle.copyWith(
+                                    color: Colors.white,
+                                    fontWeight: rubikLight,
+                                  ),
+                                ),
+                                horizontalSpaceTiny,
+                                Text(
+                                  'Japan',
+                                  style: mediumTextStyle.copyWith(
+                                      color: Colors.white,
+                                      fontWeight: rubikMedium),
+                                ),
+                                horizontalSpaceTiny,
+                                Icon(
+                                  Ionicons.location_outline,
+                                  color: Colors.white,
+                                  size: 15,
+                                )
+                              ],
+                            )
                           ],
                         ),
-                        Text(datePosted, style: smallTextStyle.copyWith(color: lLightGrey),),
-                      ],
-                    ),
-                  ),
-                  Icon(Ionicons.heart_outline, size: 35, color: lDarkGreyColor,),
-                  horizontalSpaceSmall,
-                  Icon(Ionicons.share_outline, size: 35, color: lDarkGreyColor,),
-                ],
-              ),
-            ),
-            verticalSpaceSmall,
-            ClipRect(
-              child: Stack(
-                children: [
-                  Container(
-                    height: 250,
-                    width: MediaQuery.of(context).size.width,
-                    decoration: BoxDecoration(
-                      color: Colors.grey,
-                      image: DecorationImage(
-                        image: NetworkImage('$coverImageURL'),
-                        fit: BoxFit.cover
-                      ),
-                    ),
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(horizontal: hPadding, vertical: vPadding),
-                      child: Align(
-                        alignment: Alignment.bottomLeft,
-                        child: GestureDetector(
-                            onTap: onPostTap,
-                            child: Text(title, style: GoogleFonts.merriweather(fontSize: 17, color: Colors.white),)),
                       ),
                     ),
                   ),
-                  Container(
-                    height: 250,
-                    width: MediaQuery.of(context).size.width,
-                    decoration: BoxDecoration(
-                        color: Colors.grey,
-                        gradient: LinearGradient(
-                            colors: [
-                              Colors.black.withOpacity(0.8),
-                              Colors.black.withOpacity(0.0),
-                            ],
-                            begin: FractionalOffset(0.0, 1.0),
-                            end: FractionalOffset(0.0, 0.0),
-                            stops: [0.0, 2.0],
-                            tileMode: TileMode.clamp
-                        )
-                    ),
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(horizontal: hPadding, vertical: vPadding),
-                      child: Align(
-                        alignment: Alignment.bottomLeft,
-                        child: GestureDetector(
-                            onTap: onPostTap,
-                            child: Text(title, style: GoogleFonts.merriweather(fontSize: 17, color: Colors.white),)),
+                ),
+                Positioned(
+                  bottom: 0,
+                  child: InkWell(
+                    onTap: onPostTap,
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      constraints: BoxConstraints(minHeight: 50),
+                      decoration: BoxDecoration(color: Colors.black.withOpacity(0.5)),
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: hPadding, vertical: vPadding),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text(
+                                  title,
+                                  style: mediumTextStyle.copyWith(
+                                      color: Colors.white,
+                                      fontWeight: rubikRegular),
+                                ),
+                              ),
+                            ),
+                            horizontalSpaceMedium,
+                            Icon(
+                              Ionicons.arrow_forward,
+                              color: Colors.white,
+                            )
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                ],
-              ),
+                )
+              ],
             ),
             Padding(
-              padding: EdgeInsets.symmetric(horizontal: hPadding, vertical: vPadding),
-              child: Text(content, style: GoogleFonts.merriweather(color: lDarkGreyColor), maxLines: 5, overflow: TextOverflow.ellipsis, textAlign: TextAlign.justify,),
+              padding: EdgeInsets.symmetric(
+                  horizontal: hPadding, vertical: vPadding),
+              child: Text(
+                content,
+                style: mediumTextStyle.copyWith(color: lDarkGreyColor),
+                maxLines: 5,
+                overflow: TextOverflow.ellipsis,
+                textAlign: TextAlign.justify,
+              ),
             ),
+            verticalSpaceRegular,
             Padding(
               padding: EdgeInsets.symmetric(horizontal: hPadding),
               child: Row(
                 children: [
-                  Text('2.3k Likes', style: mediumTextStyle.copyWith(color: lOtherColor, fontWeight: rubikMedium)),
+                  Text('2.3k Likes',
+                      style: mediumTextStyle.copyWith(
+                          color: lOtherColor, fontWeight: rubikMedium)),
                   horizontalSpaceRegular,
-                  Expanded(child: Text('4.1k Shares', style: mediumTextStyle.copyWith(color: lOtherColor, fontWeight: rubikMedium))),
-                  Icon(Ionicons.chatbubble_outline, color: lOtherColor,),
+                  Expanded(
+                      child: Text('4.1k Shares',
+                          style: mediumTextStyle.copyWith(
+                              color: lOtherColor, fontWeight: rubikMedium))),
+                  Icon(
+                    Ionicons.chatbubble_outline,
+                    color: lOtherColor,
+                  ),
                   horizontalSpaceTiny,
-                  Text('$commentCount', style: mediumTextStyle.copyWith(color: lOtherColor, fontWeight: rubikMedium)),
+                  Text('$commentCount',
+                      style: mediumTextStyle.copyWith(
+                          color: lOtherColor, fontWeight: rubikMedium)),
                 ],
               ),
             ),
             verticalSpaceRegular,
-            if(recentComment != null)
+            if (recentComment != null)
               Padding(
-                padding: EdgeInsets.symmetric(horizontal: horizontalMargin, vertical: 5),
+                padding: EdgeInsets.symmetric(
+                    horizontal: horizontalMargin, vertical: 5),
                 child: Row(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     CircleAvatar(
                       radius: 14,
-                      backgroundImage: NetworkImage('${UserProfile.fromJson(recentComment!.userProfile!).profileImageURL}'),
+                      backgroundImage: NetworkImage(
+                          '${UserProfile.fromJson(recentComment!.userProfile!).profileImageURL}'),
                     ),
                     horizontalSpaceSmall,
                     Expanded(
@@ -163,21 +225,30 @@ class PostItem extends StatelessWidget {
                           Row(
                             crossAxisAlignment: CrossAxisAlignment.center,
                             children: [
-                              Text('${UserProfile.fromJson(recentComment!.userProfile!).firstname} ${UserProfile.fromJson(recentComment!.userProfile!).lastname}',
-                                style: mediumTextStyle.copyWith(fontWeight: rubikMedium),),
+                              Text(
+                                '${UserProfile.fromJson(recentComment!.userProfile!).firstname} ${UserProfile.fromJson(recentComment!.userProfile!).lastname}',
+                                style: mediumTextStyle.copyWith(
+                                    fontWeight: rubikMedium),
+                              ),
                               horizontalSpaceTiny,
-                              Text('5 mins ago', style: smallTextStyle.copyWith(color: lLightGrey),),
+                              Text(
+                                '5 mins ago',
+                                style:
+                                    smallTextStyle.copyWith(color: lLightGrey),
+                              ),
                             ],
                           ),
-                          Text('${recentComment!.body}', style: mediumTextStyle.copyWith(color: lOtherColor),),
+                          Text(
+                            '${recentComment!.body}',
+                            style: mediumTextStyle.copyWith(color: lOtherColor),
+                          ),
                         ],
                       ),
                     )
                   ],
                 ),
               ),
-            if(recentComment != null)
-              verticalSpaceSmall,
+            if (recentComment != null) verticalSpaceSmall,
           ],
         ),
       ),
