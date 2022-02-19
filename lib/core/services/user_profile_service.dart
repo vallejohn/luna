27f56f97b57411ebcbe8b26f2ce442488ea60c22
vallejohn <valjohn.teruel@gmail.com>
2022-02-19 22:42:31 +0800

    removed unnecessary codes

--- lib/core/services/user_profile_service.dart
+++ lib/core/services/user_profile_service.dart
@@ -4,9 +4,6 @@ import 'package:luna/features/firebase_authentication/data/models/user_profile.d
 import 'package:rxdart/rxdart.dart';
 
 class UserProfileService{
-  late final UserProfile _userProfile;
-  UserProfile get userProfile => _userProfile;
-
   final _controller = BehaviorSubject<UserProfile>();
   Sink<UserProfile> get userSink => _controller.sink;
   Stream<UserProfile> get userStream => _controller.stream;
@@ -14,6 +11,5 @@ class UserProfileService{
 
   void setUser(UserProfile userProfile){
     userSink.add(userProfile);
-    _userProfile = userProfile;
   }
 }
