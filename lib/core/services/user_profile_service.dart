4fbea3884be41f34bce16241c5601d4ae0ab271f
vallejohn <valjohn.teruel@gmail.com>
2022-03-03 00:50:19 +0800

    Added todo comment

--- lib/core/services/user_profile_service.dart
+++ lib/core/services/user_profile_service.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:luna/features/firebase_authentication/data/models/user_profile.dart';
 import 'package:rxdart/rxdart.dart';
 
+//TODO Find a better solution to this
 class UserProfileService{
   final _controller = BehaviorSubject<UserProfile>();
   Sink<UserProfile> get userSink => _controller.sink;
