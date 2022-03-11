bfce722ea29f9ce7a51705397380bff9a42a5da5
vallejohn <valjohn.teruel@gmail.com>
2022-03-12 02:29:06 +0800

    format

--- lib/features/post/presentation/blocs/create_post/create_post_bloc.dart
+++ lib/features/post/presentation/blocs/create_post/create_post_bloc.dart
@@ -30,41 +30,45 @@ class CreatePostBloc extends Bloc<CreatePostEvent, CreatePostState> {
   FutureOr<void> _onAddPost(_OnAddPost event, Emitter<CreatePostState> emit) async{
     emit(CreatePostState.loading());
 
-    final failureOrDataState = await _uploadImage(ImageUploadParam(
-      userID: event.addPostData.user!.authID!,
-      imagePath: event.addPostData.imagePath!
-    ));
+    if(event.addPostData.imagePath != null){
+      final failureOrDataState = await _uploadImage(ImageUploadParam(
+          userID: event.addPostData.user!.authID!,
+          imagePath: event.addPostData.imagePath!
+      ));
 
-    await failureOrDataState.fold((failure) async{
-      Logger().e(failure.message.toString());
-    }, (dataState) async{
-      await dataState.when(
-          success: (imagePath) async{
-            Logger().i('Image upload success: ${imagePath.toString()}');
+      await failureOrDataState.fold((failure) async{
+        Logger().e(failure.message.toString());
+      }, (dataState) async{
+        await dataState.when(
+            success: (imagePath) async{
+              Logger().i('Image upload success: ${imagePath.toString()}');
 
-            final dataState = await _addPost(AddPostData(
-              user: event.addPostData.user,
-              content: event.addPostData.content,
-              title: event.addPostData.title,
-              imagePath: imagePath
-            ));
+              final dataState = await _addPost(AddPostData(
+                  user: event.addPostData.user,
+                  content: event.addPostData.content,
+                  title: event.addPostData.title,
+                  imagePath: imagePath
+              ));
 
-            await dataState.fold((failure) async{
-              Logger().e('Failure to upload post: ${failure.message}');
-            }, (dataState) async{
-              await dataState.when(
-                  success: (data) async{
-                    DocumentReference reference = data;
-                    Logger().i('Successfully added post: ${reference.id}');
-                    emit(CreatePostState.success(postSnapshot: reference.snapshots()));
-                  },
-                  failed: (error) async{
-                    Logger().w('Error while adding post');
-                  });
-            });
-          }, failed: (error) async{
-            Logger().w('Error uploading image: $error');
+              await dataState.fold((failure) async{
+                Logger().e('Failure to upload post: ${failure.message}');
+              }, (dataState) async{
+                await dataState.when(
+                    success: (data) async{
+                      DocumentReference reference = data;
+                      Logger().i('Successfully added post: ${reference.id}');
+                      emit(CreatePostState.success(postSnapshot: reference.snapshots()));
+                    },
+                    failed: (error) async{
+                      Logger().w('Error while adding post');
+                    });
+              });
+            }, failed: (error) async{
+          Logger().w('Error uploading image: $error');
+        });
       });
-    });
+    }else{
+      Logger().i('No image to be uploaded');
+    }
   }
 }
