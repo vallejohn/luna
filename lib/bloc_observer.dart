7dc7b90ccb69b93e81b04314b7cbbc7b6528f3bb
Valle John Teruel <valle.teruel@peddlr.ph>
2022-06-24 09:46:50 +0800

    added logs on bloc observer

--- lib/bloc_observer.dart
+++ lib/bloc_observer.dart
@@ -1,26 +1,34 @@
 import 'package:bloc/bloc.dart';
+import 'package:logger/logger.dart';
+import 'package:luna/core/utils/app_logger.dart';
 
 class AppBlocObserver extends BlocObserver {
+
+  Logger logger = AppLogger('AppBlocObserver');
+
   @override
   void onEvent(Bloc bloc, Object? event) {
+    logger.d('onEvent -- ${bloc.runtimeType}');
     super.onEvent(bloc, event);
     // TODO: implement onEvent
   }
 
   @override
   void onError(BlocBase bloc, Object error, StackTrace stacktrace) {
-    // TODO: implement onError
+    logger.e('onError -- ${bloc.runtimeType}');
     super.onError(bloc, error, stacktrace);
   }
 
   @override
   void onChange(BlocBase bloc, Change change) {
+    logger.d('onChange -- ${bloc.runtimeType}');
     super.onChange(bloc, change);
     // TODO: implement onChange
   }
 
   @override
   void onTransition(Bloc bloc, Transition transition) {
+    logger.d('onChange -- ${bloc.runtimeType}');
     super.onTransition(bloc, transition);
     // TODO: implement onChange
   }
