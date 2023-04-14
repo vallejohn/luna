d3de0f7ed24b6e1e4f328dd833d4f035146cc503
vallejohn <valjohn.teruel@gmail.com>
2023-04-15 03:27:18 +0800

    Browse image functionality

--- lib/features/firebase_authentication/presentation/pages/register_page.dart
+++ lib/features/firebase_authentication/presentation/pages/register_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -9,6 +11,7 @@ import 'package:luna/features/firebase_authentication/presentation/blocs/registe
 import 'package:luna/router/app_router.dart';
 
 import '../../../../core/utils/params.dart';
+import '../../../post/presentation/blocs/browse_image_bloc/browse_image_bloc.dart';
 import '../blocs/login/login_bloc.dart';
 import '../../../../global/custom_widgets/gradient_button.dart';
 import '../../../../global/custom_widgets/text_input_field.dart';
@@ -23,137 +26,152 @@ class RegisterPage extends StatelessWidget {
     TextEditingController emailController = TextEditingController();
     TextEditingController passwordController = TextEditingController();
 
-    return Scaffold(
-      body: BlocConsumer<RegisterBloc, RegisterState>(
-          listener: (context, state){
-            if(state.status == RegisterStatus.success){
-              Fluttertoast.showToast(msg: 'Registration success');
-            }
-            if(state.status == RegisterStatus.failed){
-              Fluttertoast.showToast(msg: state.message);
-            }
+    return MultiBlocListener(
+      listeners: [
+        BlocListener<BrowseImageBloc, BrowseImageState>(
+          listener: (context, state) {
+            state.whenOrNull(cancelled: () {
+              Fluttertoast.showToast(msg: 'Image removed');
+            });
           },
-          builder: (context, state) {
-            return Container(
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                      colors: [AppColors.primary, AppColors.blue])
-              ),
-              child: ListView(
-                children: [
-                  Align(
-                    alignment: Alignment.topLeft,
+        )
+      ],
+      child: Scaffold(
+        body: BlocConsumer<RegisterBloc, RegisterState>(
+            listener: (context, state){
+              if(state.status == RegisterStatus.success){
+                Fluttertoast.showToast(msg: 'Registration success');
+              }
+              if(state.status == RegisterStatus.failed){
+                Fluttertoast.showToast(msg: state.message);
+              }
+            },
+            builder: (context, state) {
+              return Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [AppColors.primary, AppColors.blue])
+                ),
+                child: ListView(
+                  children: [
+                    Align(
+                      alignment: Alignment.topLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 45),
+                          child: Text(
+                            'luna.',
+                            style: GoogleFonts.yesteryear(color: Colors.white, fontSize: 36),
+                          ),
+                        )),
+                    AppVerticalSpace.large,
+                    Center(
                       child: Padding(
                         padding: const EdgeInsets.symmetric(horizontal: 45),
                         child: Text(
-                          'luna.',
-                          style: GoogleFonts.yesteryear(color: Colors.white, fontSize: 36),
+                          'Create an account',
+                          style: AppTextStyle.large.copyWith(color: Colors.white, fontSize: 24, fontWeight: AppFontWeight.rubikMedium),
                         ),
-                      )),
-                  AppVerticalSpace.large,
-                  Center(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 45),
-                      child: Text(
-                        'Create an account',
-                        style: AppTextStyle.large.copyWith(color: Colors.white, fontSize: 24, fontWeight: AppFontWeight.rubikMedium),
                       ),
                     ),
-                  ),
-                  Center(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 45),
-                      child: Text(
-                        'Please provide the required information below.',
-                        style: AppTextStyle.medium.copyWith(color: Colors.white, fontWeight: AppFontWeight.rubikLight),
+                    Center(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 45),
+                        child: Text(
+                          'Please provide the required information below.',
+                          style: AppTextStyle.medium.copyWith(color: Colors.white, fontWeight: AppFontWeight.rubikLight),
+                        ),
                       ),
                     ),
-                  ),
-                  AppVerticalSpace.large,
-                  Center(
-                    child: Padding(
+                    AppVerticalSpace.large,
+                    BlocBuilder<BrowseImageBloc, BrowseImageState>(
+                      builder: (context, state) => Center(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 45),
+                          child: GestureDetector(
+                            child: Container(
+                              height: state.maybeWhen(success: (_) => 120, orElse: () => 80,),
+                              width: state.maybeWhen(success: (_) => 120, orElse: () => 80,),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(80),
+                                gradient: AppColors.linearGradient(beginColor: AppColors.accent, endColor: AppColors.yellowGreen),
+                                image: state.whenOrNull(success: (image) => DecorationImage(image: FileImage(File(image.path)), fit: BoxFit.cover),),
+                              ),
+                              child: state.maybeWhen(success: (_) => null, orElse: () => Icon(Ionicons.camera, size: 30, color: AppColors.primary[700],),),
+                            ),
+                            onTap: () => BlocProvider.of<BrowseImageBloc>(context).add(BrowseImageEvent.onBrowse()),
+                          )
+                        ),
+                      ),
+                    ),
+                    AppVerticalSpace.small,
+                    Center(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 45),
+                        child: Text(
+                          'Click the camera icon to browse a photo in your gallery.',
+                          style: AppTextStyle.medium.copyWith(color: Colors.white, fontWeight: AppFontWeight.rubikLight),
+                        ),
+                      ),
+                    ),
+                    AppVerticalSpace.large,
+                    Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 45),
-                      child: GestureDetector(
-                        child: Container(
-                          height: 80,
-                          width: 80,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(80),
-                            gradient: AppColors.linearGradient(beginColor: AppColors.accent, endColor: AppColors.yellowGreen),
+                      child: Column(
+                        children: [
+                          TextInputField(
+                            errorText: state.status == LoginStatus.failed && (state.registerError == RegisterError.emailExist)
+                                ? state.message
+                                : null,
+                            controller: emailController,
+                            hintText: 'Username',
                           ),
-                          child: Icon(Ionicons.camera, size: 30, color: AppColors.primary[700],),
-                        ),
-                      )
-                    ),
-                  ),
-                  AppVerticalSpace.small,
-                  Center(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 45),
-                      child: Text(
-                        'Click the camera icon to browse a photo in your gallery.',
-                        style: AppTextStyle.medium.copyWith(color: Colors.white, fontWeight: AppFontWeight.rubikLight),
+                          AppVerticalSpace.small,
+                          TextInputField(
+                            errorText:  null,
+                            controller: emailController,
+                            hintText: 'Display name',
+                          ),
+                          AppVerticalSpace.small,
+                          TextInputField(
+                            errorText:  null,
+                            controller: emailController,
+                            hintText: 'Email address',
+                          ),
+                          AppVerticalSpace.small,
+                          TextInputField(
+                            onShowPasswordTap: () {},
+                            obscureText: true,
+                            controller: passwordController,
+                            hintText: 'password',
+                          ),
+                          AppVerticalSpace.small,
+                          TextInputField(
+                            onShowPasswordTap: () {},
+                            obscureText: true,
+                            controller: passwordController,
+                            hintText: 'Confirm password',
+                          ),
+                        ],
                       ),
                     ),
-                  ),
-                  AppVerticalSpace.large,
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 45),
-                    child: Column(
-                      children: [
-                        TextInputField(
-                          errorText: state.status == LoginStatus.failed && (state.registerError == RegisterError.emailExist)
-                              ? state.message
-                              : null,
-                          controller: emailController,
-                          hintText: 'Username',
-                        ),
-                        AppVerticalSpace.small,
-                        TextInputField(
-                          errorText:  null,
-                          controller: emailController,
-                          hintText: 'Display name',
-                        ),
-                        AppVerticalSpace.small,
-                        TextInputField(
-                          errorText:  null,
-                          controller: emailController,
-                          hintText: 'Email address',
-                        ),
-                        AppVerticalSpace.small,
-                        TextInputField(
-                          onShowPasswordTap: () {},
-                          obscureText: true,
-                          controller: passwordController,
-                          hintText: 'password',
-                        ),
-                        AppVerticalSpace.small,
-                        TextInputField(
-                          onShowPasswordTap: () {},
-                          obscureText: true,
-                          controller: passwordController,
-                          hintText: 'Confirm password',
-                        ),
-                      ],
-                    ),
-                  ),
-                  AppVerticalSpace.medium,
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 45),
-                    child: GradientButton(
-                        text: 'Register',
-                        onLoginBusy: state.status == LoginStatus.loading,
-                        onPressed:(){
+                    AppVerticalSpace.medium,
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 45),
+                      child: GradientButton(
+                          text: 'Register',
+                          onLoginBusy: state.status == LoginStatus.loading,
+                          onPressed:(){
 
-                        }),
-                  ),
-                  AppVerticalSpace.large,
-                ],
-              ),
-            );
-          }
+                          }),
+                    ),
+                    AppVerticalSpace.large,
+                  ],
+                ),
+              );
+            }
+        ),
       ),
     );
   }
